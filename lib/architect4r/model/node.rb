96552d0047623a473bc81e89b97afd67626efe53
Maximilian Schulz <m.schulz@kulturfluss.de>
2011-10-21 13:43:32 +0200

    Refactor generic methods to be part of the persistency module instead of the node class, so we can reuse them with relationships.

--- lib/architect4r/model/node.rb
+++ lib/architect4r/model/node.rb
@@ -4,6 +4,7 @@ module Architect4r
     
     class Node
       include Architect4r::Model::Connection
+      include Architect4r::Model::Persistency
       include Architect4r::Model::Queries
       include Architect4r::Model::Relationships
       
@@ -35,12 +36,6 @@ module Architect4r
         !result.nil?
       end
       
-      # Creates the document in the db. Raises an exception
-      # if the document is not created properly.
-      def create!(options = {})
-        self.fail_validate!(self) unless self.create(options)
-      end
-      
       # Trigger the callbacks (before, after, around)
       # only if the document isn't new
       def update(options = {})
@@ -58,87 +53,12 @@ module Architect4r
         !result.nil?
       end
       
-      def save(options = {})
-        self.new? ? create(options) : update(options)
-      end
-      
-      def save!
-        self.class.fail_validate!(self) unless self.save
-        true
-      end
-      
       def destroy
         if result = connection.delete_node(self.id)
           @_destroyed = true
           self.freeze
         end
         result
-      end
-      
-      def id
-        @id ||= if raw_data && raw_data['self'].present?
-          raw_data['self'].split('/').last.to_i
-        else
-          nil
-        end
-      end
-      alias :to_key :id
-      alias :to_param :id
-      
-      def new?
-        # Persisted objects always have an id.
-        id.nil?
-      end
-      alias :new_record? :new?
-      
-      def destroyed?
-        !!@_destroyed
-      end
-      
-      def persisted?
-        !new? && !destroyed?
-      end
-      
-      # Update the document's attributes and save. For example:
-      def update_attributes(hash)
-        update_attributes_without_saving hash
-        save
-      end
-      
-      protected
-      
-      def perform_validations(options = {})
-        (options[:validate].presence || true) ? valid? : true
-      end
-      
-      
-      class << self
-        
-        def create(attributes = {}, &block)
-          instance = new(attributes, &block)
-          instance.create
-          instance
-        end
-        
-        # Defines an instance and save it directly to the database
-        def create!(attributes = {}, &block)
-          instance = new(attributes, &block)
-          instance.create!
-          instance
-        end
-        
-        protected
-        
-        # Create an object from the database data
-        def build_from_database(data)
-          return nil if data.blank?
-          
-          # Create an instance of the object
-          obj = self.new(data['data'])
-          obj.raw_data = data
-          obj
-        end
-        
       end
       
     end
--- lib/architect4r/model/persistency.rb
+++ lib/architect4r/model/persistency.rb
@@ -9,13 +9,85 @@ module Architect4r
       
       module InstanceMethods
         
+        # Creates the document in the db. Raises an exception
+        # if the document is not created properly.
+        def create!(options = {})
+          self.fail_validate!(self) unless self.create(options)
+        end
         
+        def save(options = {})
+          self.new? ? create(options) : update(options)
+        end
+
+        def save!
+          self.class.fail_validate!(self) unless self.save
+          true
+        end
+        
+        def id
+          @id ||= if raw_data && raw_data['self'].present?
+            raw_data['self'].split('/').last.to_i
+          else
+            nil
+          end
+        end
+        alias :to_key :id
+        alias :to_param :id
+
+        def new?
+          # Persisted objects always have an id.
+          id.nil?
+        end
+        alias :new_record? :new?
+
+        def destroyed?
+          !!@_destroyed
+        end
+
+        def persisted?
+          !new? && !destroyed?
+        end
+
+        # Update the document's attributes and save. For example:
+        def update_attributes(hash)
+          update_attributes_without_saving hash
+          save
+        end
+
+        protected
+
+        def perform_validations(options = {})
+          (options[:validate].presence || true) ? valid? : true
+        end
         
       end
       
       module ClassMethods
         
+        def create(*args, &block)
+          instance = new(*args, &block)
+          instance.create
+          instance
+        end
         
+        # Defines an instance and save it directly to the database
+        def create!(*args, &block)
+          instance = new(*args, &block)
+          instance.create!
+          instance
+        end
+        
+        protected
+        
+        # Create an object from the database data
+        def build_from_database(data)
+          return nil if data.blank?
+          
+          # Create an instance of the object
+          obj = self.new(data['data'])
+          obj.raw_data = data
+          obj
+        end
         
       end
     end
