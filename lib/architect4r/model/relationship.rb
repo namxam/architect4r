4fa022cb67446d022060e81bba8a35274296ae52
Maximilian Schulz <m.schulz@kulturfluss.de>
2011-10-21 13:58:13 +0200

    Add persistency to model relationships.

--- lib/architect4r/model/relationship.rb
+++ lib/architect4r/model/relationship.rb
@@ -2,15 +2,32 @@ module Architect4r
   module Model
     class Relationship
       
+      #
+      # Extensions
+      #
       include Architect4r::Model::Connection
+      include Architect4r::Model::Persistency
       
       def self.inherited(subklass)
         super
         subklass.send(:include, Architect4r::Model::Properties)
         subklass.send(:include, Architect4r::Model::Validations)
+        
+        subklass.class_exec do
+          # Validations
+          validates :source, :presence => true
+          validates :destination, :presence => true
+        end
       end
       
-      attr_accessor :source, :destination
+      #
+      # Virtual attributes
+      #
+      attr_accessor :source, :destination, :raw_data
+      
+      #
+      # Instance methods
+      #
       
       def initialize(*args)
         # Detect source and destination
@@ -26,6 +43,47 @@ module Architect4r
         if properties = args[0].is_a?(Hash) && args.shift
           parse_properties(properties)
         end
+      end
+      
+      # Create the document. Validation is enabled by default and will return
+      # false if the document is not valid. If all goes well, the document will
+      # be returned.
+      def create(options = {})
+        # only create valid records
+        return false unless perform_validations(options)
+        
+        # perform creation
+        if result = connection.create_relationship(self.source.id, self.destination.id, self.class.name, self._to_database_hash)
+          self.raw_data = result
+        end
+        
+        # if something goes wrong we receive a nil value and return false
+        !result.nil?
+      end
+      
+      # Trigger the callbacks (before, after, around)
+      # only if the document isn't new
+      def update(options = {})
+        # Check if record can be updated
+        raise "Cannot save a destroyed document!" if destroyed?
+        raise "Calling #{self.class.name}#update on document that has not been created!" if new?
+        
+        # Check if we can continue
+        return false unless perform_validations(options)
+        
+        # perform update
+        result = connection.update_relationship(self.id, self._to_database_hash)
+        
+        # if something goes wrong we receive a nil value and return false
+        !result.nil?
+      end
+      
+      def destroy
+        if result = connection.delete_relationship(self.id)
+          @_destroyed = true
+          self.freeze
+        end
+        result
       end
       
     end
--- spec/model/relationship_spec.rb
+++ spec/model/relationship_spec.rb
@@ -5,12 +5,6 @@ describe Architect4r::Model::Relationship do
   let(:person) { Person.create(:name => 'Niobe', :human => true) }
   let(:ship) { Ship.create(:name => 'Logos', :max_crew_size => 2) }
   
-  it "should work" do
-    pending
-    fs = CrewMembership.new(:member_since => DateTime.new, :rank => 'Captain')
-    fs.valid?.should be_false
-  end
-  
   describe "initialization of new instances" do
     
     it "should accept zero arguments" do
@@ -33,6 +27,34 @@ describe Architect4r::Model::Relationship do
       ms.source.should == ship
       ms.destination.should == person
       ms.rank.should == 'Captain'
+    end
+    
+    it "should always require a source and destination" do
+      fs = CrewMembership.new(:member_since => DateTime.new, :rank => 'Captain')
+      fs.valid?.should be_false
+    end
+    
+  end
+  
+  describe "Creating a new relationship" do
+    
+    it "should create a new relationship" do
+      ms = CrewMembership.new(ship, person, { :rank => 'Captain' })
+      ms.save.should be_true
+    end
+    
+    it "should allow changing relationship properties" do
+      ms = CrewMembership.new(ship, person, { :rank => 'Captain' })
+      ms.save.should be_true
+      
+      ms.rank = 'Operator'
+      ms.save.should be_true
+    end
+    
+    it "should allow deleting relationships" do
+      ms = CrewMembership.new(ship, person, { :rank => 'Captain' })
+      ms.save.should be_true
+      ms.destroy.should be_true
     end
     
   end
