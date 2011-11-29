6c454b76e7a4b8eee6d841aef91ed8af7f3a378d
Dmytrii Nagirniak <dnagir@gmail.com>
2011-11-30 09:58:49 +1100

    Refactor spec a bit

--- spec/model/links_query_interface_spec.rb
+++ spec/model/links_query_interface_spec.rb
@@ -7,16 +7,7 @@ describe Architect4r::Model::LinksQueryInterface do
   
   subject { Architect4r::Model::LinksQueryInterface.new(@node) }
   
-  it "should respond to :incoming" do
-    subject.should respond_to(:incoming)
-  end
-  
-  it "should respond to :incoming" do
-    subject.should respond_to(:outgoing)
-  end
-  
-  it "should respond to :incoming" do
-    subject.should respond_to(:both)
-  end
-  
+  it { should respond_to :incoming }
+  it { should respond_to :outgoing }
+  it { should respond_to :both }
 end
